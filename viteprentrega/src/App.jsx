779ae1010d2f66e4a7d7a27120f2ddfc5e2e9358
nachopasut <nachopasut9@gmail.com>
2023-11-20 12:05:19 -0300

    exportando

--- viteprentrega/src/App.jsx
+++ viteprentrega/src/App.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
-import ItemListContainer from './components/ItemListContainer';
+import ItemListContainer from './components/itemListContainer'; 
 import ItemCount from './components/ItemCount';
+import ItemDetailCointainer from './components/ItemDetailCointainer'
 
 function App() {
   return (
@@ -13,10 +14,11 @@ function App() {
         <Routes>
           <Route path='/' element={<ItemListContainer />} />
           <Route path='/category/:categoryId' element={<ItemListContainer />} />
-          <Route path='/item/:itemId' element={<ItemListContainer />} />
+          <Route path='/item/:itemId' element={<ItemDetailCointainer />} />
           <Route path='*' element={<h1>Not Found</h1>} />
+          
         </Routes>
-       
+      
       </BrowserRouter>
     </div>
   );
--- viteprentrega/src/components/NavBar.jsx
+++ viteprentrega/src/components/NavBar.jsx
@@ -3,13 +3,16 @@ import Card from "./CartWidget"
 const NavBar = () => {
   return (
     <nav className="NavBar">
-      <link to ='/'>
+      <Link to ='/'>
         <h3>Ecommerce</h3>
-      </link>
+      </Link>
         <div className="Categories">
-          <Navlink to={'/category/celular'}className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}></Navlink>
-          <Navlink to={'/category/consola'}className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}></Navlink>
-          <Navlink to={'/category/pc'}className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}></Navlink>
+          <Navlink to=
+          {'/category/celular'}className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}>
+            
+          a</Navlink>
+          <Navlink to={'/category/consola'}className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}>B</Navlink>
+          <Navlink to={'/category/pc'}className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}>C</Navlink>
         
           <Card/>
           
