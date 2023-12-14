dd7b6f2764731483a148beb4b71e9a697d4540af
nachopasut <nachopasut9@gmail.com>
2023-12-14 14:00:58 -0300

    solucionando problmas

--- viteprentrega/src/components/FireBaseConfig.jsx
+++ viteprentrega/src/components/FireBaseConfig.jsx
@@ -1,5 +1,6 @@
-import {initializeApp} from "firebase/app"
-import {getFirestore} from "firebase/firestore"
+import { initializeApp } from "firebase/app";
+import { getFirestore, Timestamp } from "firebase/firestore";
+
 const firebaseConfig = {
     apiKey: "AIzaSyDZ9L4KIJ4UEb4J5SdUoKR84dqJQek7sFU",
     authDomain: "entrega-final-3cdb1.firebaseapp.com",
@@ -9,5 +10,7 @@ const firebaseConfig = {
     appId: "1:624672405477:web:8b338f242a74d61d52610c",
     measurementId: "G-E1E6VFXPN6"
   };
-  const app = initializeApp (firebaseConfig)
-  export const db = getFirestore
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  
+  export { db, Timestamp };
